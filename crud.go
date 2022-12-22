b67bdf3350f1ae4972e6d501b286dd31c7d576c3
RodrigoRocha07 <rodrigorochafn@gmail.com>
2022-12-22 17:56:16 -0300

    crud2.0

--- crud.go
+++ crud.go
@@ -5,7 +5,7 @@ import "fmt"
 func main() {
 	nomes := []string{}
 	for {
-		fmt.Println(" 1 - Listar \n 2 - Acrescentar \n 3 - Atualizar \n 4 - Deletar \n 0 - Finalizar")
+		fmt.Println(" 1 - Listar \n 2 - Acrescentar \n 3 - Atualizar \n 4 - Deletar \n 5 - Deletar varios \n 0 - Finalizar")
 		comando := 0
 		fmt.Scan(&comando)
 		if comando == 1 {
@@ -15,10 +15,16 @@ func main() {
 				fmt.Println(nomes)
 			}
 		} else if comando == 2 {
-			fmt.Println("Digite o nome: ")
-			var novoNome string
-			fmt.Scan(&novoNome)
-			nomes = append(nomes, novoNome)
+			fmt.Println("Quantos nomes deseja acrescentar?")
+			var nAcrescentar int
+			fmt.Scan(&nAcrescentar)
+			for i := 0; i < nAcrescentar; i++ {
+				fmt.Println("Digite o nome: ")
+				var novoNome string
+				fmt.Scan(&novoNome)
+				nomes = append(nomes, novoNome)
+			}
+
 		} else if comando == 3 {
 			if len(nomes) == 0 {
 				fmt.Println("Nenhum nome para atualizar!")
@@ -48,6 +54,26 @@ func main() {
 
 				nomes = append(nomes[:deletar], nomes[deletar+1:]...)
 			}
+		} else if comando == 5 {
+			fmt.Println("Quantos elementos deseja deletar? ")
+			var nDelete int
+			fmt.Scan(&nDelete)
+			if nDelete > len(nomes) {
+				fmt.Println("Valor maior do que a quantidade de nomes!")
+			} else {
+				for i := 0; i < nDelete; i++ {
+					for i := 0; i < len(nomes); i++ {
+						fmt.Println(i, "-", nomes[i])
+					}
+					var deletar int
+					fmt.Scan(&deletar)
+
+					nomes = append(nomes[:deletar], nomes[deletar+1:]...)
+
+				}
+
+			}
+
 		} else {
 			break
 		}
